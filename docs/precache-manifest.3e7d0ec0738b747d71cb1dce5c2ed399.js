e9479e9cc9d219c3d145809825f05d4318a034ca
ShoneSingLone <ShoneSingLone@gmail.com>
2020-11-01 13:40:11 +0800

    deploy

---
+++ docs/precache-manifest.3e7d0ec0738b747d71cb1dce5c2ed399.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3717988a7cc78775687b",
+    "url": "css/app.bb5c9646.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4f62a40cf038058ba22a8e840fc3901b",
+    "url": "index.html"
+  },
+  {
+    "revision": "e31264a839ff357db8ac",
+    "url": "js/about.d3023212.js"
+  },
+  {
+    "revision": "3717988a7cc78775687b",
+    "url": "js/app.2e52eac5.js"
+  },
+  {
+    "revision": "f30428155bd4c1437a11",
+    "url": "js/chunk-vendors.ebe0c8ef.js"
+  },
+  {
+    "revision": "8c32b6cac972dd17c5b02463ba73e1f4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
