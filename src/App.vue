d71340b366dc7786c0a8e2f4bc766b6f6baa5757
ShoneSingLone <ShoneSingLone@gmail.com>
2020-11-01 13:32:27 +0800

    clean

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="nav">
+    <div class="btn">btn</div>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    publicPath: "./",
+    outputDir: "docs",
+    runtimeCompiler: true,
+    devServer: {
+        open: true,
+    }
+};
